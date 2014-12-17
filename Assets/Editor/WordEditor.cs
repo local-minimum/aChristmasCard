8854615e66591cf720550710bb5984f8f45980fc
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-12-17 02:24:12 +0100

    WordPropertyEditor Works

--- Assets/Editor/WordEditor.cs
+++ Assets/Editor/WordEditor.cs
@@ -5,26 +5,36 @@ using UnityEditor;
 [CustomPropertyDrawer(typeof(Word))]
 public class WordEditor : PropertyDrawer {
 
+	SerializedProperty curProp;
+
 	public override void OnGUI (Rect position, SerializedProperty property, GUIContent label)
 	{
+
+		curProp = property;
 		string name = property.FindPropertyRelative("word").stringValue;
 		if (name != "")
 			label.text = name;
-		EditorGUI.PropertyField(position, property.FindPropertyRelative("__inspectorExpanded"), GUIContent.none);
-		position.x += 20f;
-		EditorGUI.PrefixLabel(position, label);
-		if (property.FindPropertyRelative("__inspectorExpanded").boolValue) {
+		property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label, true);
+		if (property.isExpanded) {
 			int indentLvl =	EditorGUI.indentLevel;
 			Rect contentPosition = EditorGUI.IndentedRect(position);
 			EditorGUI.indentLevel = 0;
-			contentPosition.y += GetPropertyHeight(property, label);
+			contentPosition.y += base.GetPropertyHeight(property, label);
+			contentPosition.height /= 4;
 			EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("word"));
-			contentPosition.y += GetPropertyHeight(property, label);
+			contentPosition.y += base.GetPropertyHeight(property, label);
 			EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("knownVersion"));
-			contentPosition.y += GetPropertyHeight(property, label);
+			contentPosition.y += base.GetPropertyHeight(property, label);
 			EditorGUI.PropertyField(contentPosition, property.FindPropertyRelative("unknownVersion"));
 			EditorGUI.indentLevel = indentLvl;
 
 		}
+
+	}
+
+	public override float GetPropertyHeight (SerializedProperty property, GUIContent label) {
+		if (curProp == property)
+			return base.GetPropertyHeight(property, label);
+		return base.GetPropertyHeight(property, label) * (property.isExpanded ? 4 : 1);
 	}
 }
