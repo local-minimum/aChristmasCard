961847b3318a10b880daf7575d864f52c17b71b3
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-12-19 12:23:37 +0100

    door open

---
+++ Assets/Scripts/InterestPointTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class InterestPointTrigger : InterestPoint {
+
+	public bool loop = false;
+	public bool loopBounce = false;
+
+	public List<string> triggerSequence = new List<string>();
+
+	public Animator animator;
+
+	private int nextTrigger = 0;
+	private int addToNext = 1;
+
+
+	void Start() {
+		if (animator == null)
+			animator = GetComponentInParent<Animator>();
+	}
+
+	public override void Action (PlayerController player)
+	{
+		if (nextTrigger >= triggerSequence.Count) {
+			if (loop) {
+				addToNext *= -1;
+				nextTrigger += 2 * addToNext;
+			} else {
+				return;
+			}
+		}
+
+		animator.SetTrigger(triggerSequence[nextTrigger]);
+
+		nextTrigger += addToNext;
+	}
+}
