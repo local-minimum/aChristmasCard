fd7f5b4f63d018a3eff718fa2db663cc75eb2194
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-12-19 17:22:26 +0100

    Refusing to go into dark room without flashlight

---
+++ Assets/Scripts/InterestPointDoorOption.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InterestPointDoorOption : InterestingPointDoor {
+
+	private int pathDirection = 1;
+	public string learnWordFail = "";
+	public string learnWordSucceed = "";
+
+	protected override void Update ()
+	{
+		if (inTransition) {
+			
+			if (player.CheckProximity(passageTrail[pathIndex].position, pathIndex == passageTrail.Count - 1)) {
+				pathIndex += pathDirection;
+				if (pathIndex >= passageTrail.Count) {
+					if (player.hasLight) {
+						UnlockPlayer(altViewedFrom);
+						player.Learn(learnWordSucceed);
+						return;
+					} else {
+						pathDirection *= -1;
+						pathIndex += pathDirection;
+						player.Learn(learnWordFail);
+					}
+				} else if (pathIndex < 0) {
+					UnlockPlayer(viewedFrom);
+				}
+			}
+			
+			player.Move(player.GetAim(passageTrail[pathIndex].position));
+		}
+	}
+}
--- Assets/Scripts/InterestingPointDoor.cs
+++ Assets/Scripts/InterestingPointDoor.cs
@@ -5,9 +5,10 @@ public class InterestingPointDoor : InterestPointTrigger {
 	public InterestPoint altViewedFrom;
 	public List<Transform> passageTrail = new List<Transform>();
 	public string closeTrigger = "Close";
-	bool inTransition = false;
-	int pathIndex = 0;
-	PlayerController player;
+	protected bool inTransition = false;
+	protected int pathIndex = 0;
+	protected PlayerController player;
+
 	public override void SpecificAction (PlayerController player)
 	{
 		player.playerLocked = true;
@@ -15,31 +16,31 @@ public class InterestingPointDoor : InterestPointTrigger {
 		inTransition = true;
 	}
 
-	void UnlockPlayer() {
+	protected void UnlockPlayer(InterestPoint pt) {
 
-		InterestPoint pt = altViewedFrom;
-		altViewedFrom = viewedFrom;
-		viewedFrom = pt;
-		passageTrail.Reverse();
+		if (pt != viewedFrom) {
+			altViewedFrom = viewedFrom;
+			viewedFrom = pt;
+			passageTrail.Reverse();
+			player.room = viewedFrom.room;
+			room = viewedFrom.room;
+		}
 		animator.SetTrigger(closeTrigger);
-		player.room = viewedFrom.room;
-		room = viewedFrom.room;
+
 		player.SetTargetPath(viewedFrom);
 		player.playerLocked = false;
 		player.moveable = true;
-//		player.target = viewedFrom;
-//		player = null;
 		inTransition = false;
 		pathIndex = 0;
 	}
 
-	void Update() {
+	protected virtual void Update() {
 		if (inTransition) {
 
 			if (player.CheckProximity(passageTrail[pathIndex].position, pathIndex == passageTrail.Count - 1)) {
 				pathIndex++;
 				if (pathIndex >= passageTrail.Count) {
-					UnlockPlayer();
+					UnlockPlayer(altViewedFrom);
 					return;
 				}
 			}
