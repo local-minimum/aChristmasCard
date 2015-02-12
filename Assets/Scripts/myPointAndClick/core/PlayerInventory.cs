d9ca8912fb08d4e77f2dd7db18b148ed232fe690
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-02-12 14:16:43 +0100

    Renamed inventoryshape and added sprite for inventory

--- Assets/Scripts/myPointAndClick/core/PlayerInventory.cs
+++ Assets/Scripts/myPointAndClick/core/PlayerInventory.cs
@@ -6,5 +6,12 @@ namespace PointClick {
 	public class PlayerInventory : PlayerAspect {
 
 
+		public bool PickUp(GameObject item) {
+			return false;
+		}
+
+		public GameObject Drop(GameObject item) {
+			return item;
+		}
 	}
 }
