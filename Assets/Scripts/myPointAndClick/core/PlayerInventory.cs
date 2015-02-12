a52f41b57f4518e951592c951075f1f6b86e7edc
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-02-12 15:22:39 +0100

    Started on pickup logic

--- Assets/Scripts/myPointAndClick/core/PlayerInventory.cs
+++ Assets/Scripts/myPointAndClick/core/PlayerInventory.cs
@@ -1,17 +1,36 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace PointClick {
 
 	public class PlayerInventory : PlayerAspect {
 
+		public string[] allowTags = new string[0];
 
+		public int[] inventoryShape;
+
+		private Dictionary<GameObject, Interactable> slots = new Dictionary<GameObject, Interactable>();
+			
 		public bool PickUp(GameObject item) {
-			return false;
+
+			Interactable interactable = item.GetComponentInChildren<Interactable>();
+
+			if (interactable && interactable.pocketable && (allowTags.Length == 0 || allowTags.Contains(item.tag))) {
+				return _take(item, interactable);
+			} else
+				return false;
 		}
 
 		public GameObject Drop(GameObject item) {
 			return item;
 		}
+
+		bool _take(GameObject item, Interactable interactable) {
+
+			return true;
+		}
+	
+
 	}
 }
