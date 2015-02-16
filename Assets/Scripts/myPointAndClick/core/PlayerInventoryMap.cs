30b4fb1aac86835b6630a9e2cbc86cf828967514
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-02-16 13:32:42 +0100

    Inventories now have pickup and drop methods

--- Assets/Scripts/myPointAndClick/core/PlayerInventoryMap.cs
+++ Assets/Scripts/myPointAndClick/core/PlayerInventoryMap.cs
@@ -4,6 +4,27 @@ using System.Linq;
 
 namespace PointClick {
 
+	public struct InventoryLayout {
+
+		public InventoryVector shape;
+		public int[,,] grid;
+
+		public int this[InventoryVector pos] {
+			get {
+				return grid[pos.x, pos.y, pos.z];
+			}
+
+			set {
+				grid[pos.x, pos.y, pos.z] = value;
+			}
+		}
+
+		public InventoryLayout(int[] shapeArray) {
+			shape = new InventoryVector(shapeArray);
+			grid = new int[shape.x, shape.y, shape.z];
+		}
+	}
+
 	public class InventoryFilter : object {
 
 		Dictionary<string, bool> tags = new Dictionary<string, bool>();
@@ -44,40 +65,24 @@ namespace PointClick {
 		}
 	}
 
-	public struct InventoryPosition {
-		public int[] anchor;
-		public bool rotated;
-
-		public InventoryPosition(int[] anchor) {
-			this.anchor = anchor;
-			rotated = false;
-		}
-
-		public InventoryPosition(int[] anchor, bool rotated) {
-			this.anchor = anchor;
-			this.rotated = rotated;
-		}
-
-		public bool valid {
-			get {
-				return anchor.Length > 0;
-			}
-		}
-	}
-
 	public struct InventoriedItem {
 		public GameObject item;
 		public Interactable interactable;
-		public InventoryPosition position;
+		public InventoryVector position;
+		public InventoryVector shape;
 
-		public InventoriedItem(GameObject item, Interactable interactable, InventoryPosition position) {
+		public InventoriedItem(GameObject item, Interactable interactable, InventoryVector position) {
 			this.item = item;
 			this.interactable = interactable;
 			this.position = position;
+			this.shape = new InventoryVector(interactable.inventoryShape);
 		}
 	}
-
+	
 	public struct InventoryVector {
+
+		private static InventoryVector _outOfBounds = new InventoryVector(-1, -1, -1);
+
 		public int x;
 		public int y;
 		public int z;
@@ -87,15 +92,36 @@ namespace PointClick {
 			this.y = y;
 			this.z = z;
 		}
+
+		public InventoryVector(int[] shapeArray) {
+			shapeArray = InventoryVector._PrepareArrayForInventoryVector(shapeArray).ToArray();
+			this.x = shapeArray[0];
+			this.y = shapeArray[1];
+			this.z = shapeArray[2];
+		}
+
+		public static InventoryVector Invalid {
+			get {
+				return _outOfBounds;
+			}
+		}
+
+		static IEnumerable<int> _PrepareArrayForInventoryVector(int[] arr) {
+			for (int i=0; i<3; i++)
+				yield return i < arr.Length ? arr[i] : 1;
+		}
+
+		public bool valid {
+			get {
+				return x >= 0 && y >= 0 && z >= 0;
+			}
+		}
 	}
 
 	public class PlayerInventoryMap : object {
 
 		[SerializeField]
-		private InventoryVector _shape = new InventoryVector();
-
-		[SerializeField]
-		private int[,,] layout;
+		private InventoryLayout layout;
 
 		[SerializeField]
 		private InventoryFilter _tagFilter;
@@ -110,37 +136,48 @@ namespace PointClick {
 		}
 
 		public PlayerInventoryMap(int[] shape, string[] tags) {
-			_shape = AsInventoryVector(shape);
-			layout = new int[_shape.x,_shape.y,_shape.z];
+			layout = new InventoryLayout(shape);
 			_tagFilter = new InventoryFilter(tags);
 		}
 
 		public bool CanTake(Interactable interactable) {
 
-			return tagFilter.hasCommon(interactable.inventoryTags) && fits(AsInventoryVector(
+			return tagFilter.hasCommon(interactable.inventoryTags) && fits(new InventoryVector(
 				interactable.inventoryShape));
 			
 		}
 
 		public void Take(GameObject item, Interactable interactable) {
-			InventoryPosition pos = FindFirstPosition(AsInventoryVector(interactable.inventoryShape));
-			if (pos.valid) {
+			InventoryVector shape = new InventoryVector(interactable.inventoryShape);
+			InventoryVector anchor = FindFirstPosition(shape);
+			if (anchor.valid) {
 				int itemId = GetNewItemIdentifier();
-				items.Add(itemId, new InventoriedItem(item, interactable, pos));
-
+				items.Add(itemId, new InventoriedItem(item, interactable, anchor));
+				takeAt(anchor, shape, itemId);
 			}
 		}
 
-		InventoryPosition FindFirstPosition(InventoryVector shape) {
-			for (int x=0; x<=_shape.x - shape.x; x++) {
-				for (int y=0; y<=_shape.y - shape.y; y++) {
-					for (int z=0; z<=_shape.z - shape.z; z++) {
-						if (empty(new InventoryVector(x, y, z), shape))
-							return new InventoryPosition(new int[3] {x, y, z});
+		public GameObject Drop(InventoryVector position) {
+			int itemId = layout[position];
+			if (itemId > 0) {
+				InventoriedItem item = items[itemId];
+				items.Remove(itemId);
+				takeAt(item.position, item.shape, 0);
+				return item.item;
+			}
+			return null;
+		}
+
+		InventoryVector FindFirstPosition(InventoryVector shape) {
+			for (int x=0; x<=layout.shape.x - shape.x; x++) {
+				for (int y=0; y<=layout.shape.y - shape.y; y++) {
+					for (int z=0; z<=layout.shape.z - shape.z; z++) {
+						if (fitsAt(new InventoryVector(x, y, z), shape))
+							return new InventoryVector(new int[3] {x, y, z});
 					}
 				}
 			}
-			return new InventoryPosition();
+			return InventoryVector.Invalid;
 		}
 
 		int GetNewItemIdentifier() {
@@ -162,10 +199,19 @@ namespace PointClick {
 			}
 		}
 
-		bool empty(InventoryVector origin, InventoryVector shape) {
+		void takeAt(InventoryVector origin, InventoryVector shape, int value) {
+			foreach (InventoryVector coord in coordLister(origin, shape)) {
+				if (value != 0 && layout[coord] != 0)
+					Debug.LogWarning("Two overlapping items in inventory");
+				layout[coord] = value;
+
+			}
+		}
+
+		bool fitsAt(InventoryVector origin, InventoryVector shape) {
 
 			foreach (InventoryVector coord in coordLister(origin, shape)) {
-				if (layout[coord.x, coord.y, coord.z] != 0)
+				if (layout[coord] != 0)
 					return false;
 			}
 			return true;
@@ -173,16 +219,6 @@ namespace PointClick {
 
 		private bool fits(InventoryVector shape) {
 			return FindFirstPosition(shape).valid;
-		}
-
- 		InventoryVector AsInventoryVector(int[] arr) {
-			arr = _PrepareArrayForInventoryVector(arr).ToArray();
-			return new InventoryVector(arr[0], arr[1], arr[2]);
-		}
-		
-		IEnumerable<int> _PrepareArrayForInventoryVector(int[] arr) {
-			for (int i=0; i<3; i++)
-				yield return i < arr.Length ? arr[i] : 1;
 		}
 
 	}
